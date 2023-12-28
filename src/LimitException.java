830218f938afdbb411a9aebead3cf7b3aefa5339
BolotbekovvB <149691592+BolotbekovvB@users.noreply.github.com>
2023-12-28 15:19:57 +0600

    LimitException.java

--- src/LimitException.java
+++ src/LimitException.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class LimitException {
+public class LimitException extends Exception{
+    private  double remainingAmount;
+
+    public double getRemainingAmount() {
+        return remainingAmount;
+    }
+    public LimitException(String message,double remainingAmount){
+        super(message);
+        this.remainingAmount = remainingAmount;
+    }
 }
