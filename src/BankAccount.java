3ac4829b54aee94ce6f6b68f44085d5753f86e8c
BolotbekovvB <149691592+BolotbekovvB@users.noreply.github.com>
2023-12-28 15:20:28 +0600

    BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+    public void deposit(double sum){
+        amount += sum;
+        System.out.println("положить деньги на счет - " + sum);
+    }
+    public void withDraw(int sum) throws LimitException{
+        if (amount < sum ) {
+            throw new LimitException("На вашем счету недостаточно средств: ", amount);
+        }else {
+            System.out.println("Вы можете снять со счета: " + sum);
+            amount -= sum;
+            System.out.println("Вы сняли со счета: " + sum);
+        }
+    }
 }
